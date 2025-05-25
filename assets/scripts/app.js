b0ca6b7ec24b93ecb527ab32e32d0b4d20366c38
Andrey Aires <andrey.aires@gmail.com>
2025-05-25 18:46:38 -0300

    class3: Functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,31 @@
 // console.log("API Key:", util.default);
 // console.log("API Key 1:", api);
 
-let apiKey1 = ('b' + 'a' + + 'a' + 'a').toLowerCase(); //mutable
-const apiKey = ('b' + 'a' + + 'a' + 'a').toUpperCase(); //read only
+// let apiKey1 = ('b' + 'a' + + 'a' + 'a').toLowerCase(); //mutable
+// const apiKey = ('b' + 'a' + + 'a' + 'a').toUpperCase(); //read only
 
-console.log(`${apiKey1}`); // "banana"
-console.log(apiKey); // "BANANA"
+// console.log(`${apiKey1}`); // "banana"
+// console.log(apiKey); // "BANANA"
+
+
+// Functions
+
+// function getRandomInt(min, max, fixed = false) {
+//     if (fixed) {
+//         min = Math.ceil(min);
+//         max = Math.floor(max);
+//     }
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// const randomInt = getRandomInt(1, 9999, true); // Example usage: generates a random integer between 1 and 10
+// console.log(randomInt); // Example usage: generates a random integer between 1 and 10
+
+// Arrow Functions
+const add = (a, b) => a + b;
+const sub = (a, b) => { return a - b; };
+const mirror = a => a;
+const object = (a, b) => ({ a: a, b: b });
+
+console.log(mirror("c"));
+console.log(object("1", "2").b);
