9409e291e24bc275bcf239fd6bba1875ee148d18
Andrey Aires <andrey.aires@gmail.com>
2025-05-25 18:24:11 -0300

    class1: Import Export

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+import * as util from "./util.js";
+import { apiKey1 as api } from "./util.js";
+
+console.log("API Key:", util.default);
+console.log("API Key 1:", api);
---
+++ assets/scripts/util.js
@@ -0,0 +1,5 @@
+export let apiKey1 = "apiKey1";
+export let apiKey2 = "apiKey2";
+export let apiKey3 = "apiKey3";
+
+export default "default";
