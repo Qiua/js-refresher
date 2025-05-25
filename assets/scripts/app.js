16be199a7e9ad550809de36b4311c9176b6d28fa
Andrey Aires <andrey.aires@gmail.com>
2025-05-25 18:34:42 -0300

    class2: Variables and Values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,11 @@
-import * as util from "./util.js";
-import { apiKey1 as api } from "./util.js";
+// import * as util from "./util.js";
+// import { apiKey1 as api } from "./util.js";
 
-console.log("API Key:", util.default);
-console.log("API Key 1:", api);
+// console.log("API Key:", util.default);
+// console.log("API Key 1:", api);
+
+let apiKey1 = ('b' + 'a' + + 'a' + 'a').toLowerCase(); //mutable
+const apiKey = ('b' + 'a' + + 'a' + 'a').toUpperCase(); //read only
+
+console.log(`${apiKey1}`); // "banana"
+console.log(apiKey); // "BANANA"
